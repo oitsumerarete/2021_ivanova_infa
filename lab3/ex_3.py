c7de752cd3daef2145343146e289b375062ae794
Abrackadabrr <iliynea2002@ya.ru>
2021-02-26 21:45:08 +0300

    Добавил коту свойство спящий

--- lab3/ex_3.py
+++ lab3/ex_3.py
@@ -2,6 +2,7 @@ import pygame
 import pygame.draw as draw
 from math import pi
 from math import sin
+
 pygame.init()
 
 FPS = 30
@@ -18,6 +19,7 @@ LIGHT_COLOR_WINDOW = (197, 241, 233)
 DARK_COLOR_WINDOW = (25, 35, 25)
 NIGHT_EYE_COLOR = 'GREEN'
 LIGHT_EYE_COLOR = (60, 150, 60)
+SLEEP_EYE_COLOR = (235, 130, 51)
 
 
 def window(x0, y0, width, length, day):
@@ -32,6 +34,7 @@ def window(x0, y0, width, length, day):
         color = LIGHT_COLOR_WINDOW
     else:
         color = DARK_COLOR_WINDOW
+
     draw.rect(screen, (65, 56, 65), (x0, y0, width, length))
     draw.rect(screen, color, (x0 + 0.05 * width, y0 + 0.045 * length, 0.425 * width, 0.25 * length))
     draw.rect(screen, color, (x0 + 0.53 * width, y0 + 0.045 * length, 0.425 * width, 0.25 * length))
@@ -39,7 +42,7 @@ def window(x0, y0, width, length, day):
     draw.rect(screen, color, (x0 + 0.53 * width, y0 + 0.34 * length, 0.425 * width, 0.59 * length))
 
 
-def cat(x0, y0, length, width, day):
+def cat(x0, y0, length, width, day, active_in_night=True):
     '''
     :param x0: координата верхнего левого угла кошки по оХ
     :param y0: координата верхнего левого угла кошки по оУ
@@ -94,21 +97,27 @@ def cat(x0, y0, length, width, day):
     draw.polygon(screen, 'black',
                  [(x0 + 0.084 * length, y0 + 0.075 * width), (x0 + 0.047 * length, y0 + 0.14 * width),
                   (x0 + 0.041 * length, y0 + 0.04 * width)], 1)
+
     EYE_COLOR = LIGHT_EYE_COLOR
+
     if not day:
-        EYE_COLOR = NIGHT_EYE_COLOR
+        if active_in_night:
+            EYE_COLOR = NIGHT_EYE_COLOR
+        else:
+            EYE_COLOR = SLEEP_EYE_COLOR
+
     draw.ellipse(screen, EYE_COLOR, (x0 + 0.196 * length, y0 + 0.175 * width, 0.07 * length, 0.15 * width))
     draw.ellipse(screen, 'black', (x0 + 0.196 * length, y0 + 0.175 * width, 0.07 * length, 0.15 * width), 1)
-    draw.ellipse(screen, 'black', (x0 + 0.24 * length, y0 + 0.2 * width, 0.014 * length, 0.1 * width))
-    blick = pygame.Surface((0.042 * length, 0.025 * width))
-    blick.set_colorkey('BLACK')
-    draw.ellipse(blick, 'white', (0, 0, 0.042 * length, 0.025 * width))
-    blick2 = pygame.transform.rotate(blick, 110)
-    screen.blit(blick2, (x0 + 0.21 * length, y0 + 0.2 * width))
     draw.ellipse(screen, EYE_COLOR, (x0 + 0.07 * length, y0 + 0.175 * width, 0.07 * length, 0.15 * width))
     draw.ellipse(screen, 'black', (x0 + 0.07 * length, y0 + 0.175 * width, 0.07 * length, 0.15 * width), 1)
-    draw.ellipse(screen, 'black', (x0 + 0.11 * length, y0 + 0.2 * width, 0.014 * length, 0.1 * width))
-    screen.blit(blick2, (x0 + 0.084 * length, y0 + 0.2 * width))
+    if active_in_night or day:
+        draw.ellipse(screen, 'black', (x0 + 0.24 * length, y0 + 0.2 * width, 0.014 * length, 0.1 * width))
+        blick = pygame.Surface((0.042 * length, 0.025 * width))
+        blick.set_colorkey('BLACK')
+        draw.ellipse(blick, 'white', (0, 0, 0.042 * length, 0.025 * width))
+        screen.blit(pygame.transform.rotate(blick, 110), (x0 + 0.21 * length, y0 + 0.2 * width))
+        draw.ellipse(screen, 'black', (x0 + 0.11 * length, y0 + 0.2 * width, 0.014 * length, 0.1 * width))
+        screen.blit(pygame.transform.rotate(blick, 110), (x0 + 0.084 * length, y0 + 0.2 * width))
 
     draw.polygon(screen, (243, 163, 238),
                  [(x0 + 0.154 * length, y0 + 0.35 * width), (x0 + 0.18 * length, y0 + 0.35 * width),
@@ -143,6 +152,13 @@ def knit(x0, y0, r, surface):
     # d.arc(surface, 'grey', (x0 - 1.875 * r, y0 - 0.5 * r, 1.75 * r, 1.87 * r), 1.3 * pi, 1.8 * pi)
     # d.arc(surface, 'grey', (x0 - 3.225 * r, y0 + 0.9 * r, 2 * r, 1.5 * r), 0.2 * pi, 0.8 * pi)
 
+def rollknit(counter_of_ticks, knit_speed):
+    roll_knit = pygame.Surface((50, 50))
+    knit(25, 25, 25, roll_knit)
+    roll_knit.set_colorkey((0, 0, 0))
+    screen.blit(pygame.transform.rotate(roll_knit, (-1) * counter_of_ticks * knit_speed),
+                (counter_of_ticks * knit_speed, 600 - 50 * sin(counter_of_ticks / 3)))
+
 
 finished = False
 day = True
@@ -151,7 +167,6 @@ day = True
 draw.rect(screen, (128, 85, 23), (0, 0, 600, 350))
 draw.rect(screen, (195, 144, 20), (0, 350, 600, 450))
 
-roll_knit = pygame.Surface((50, 50))
 knit_speed = 10
 counter_of_ticks = 0
 
@@ -164,17 +179,17 @@ while not finished:
             finished = True
         if event.type == pygame.KEYDOWN:
             day = not day
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            rollknit(counter_of_ticks, knit_speed)
     draw.rect(screen, (128, 85, 23), (0, 0, 600, 350))
     draw.rect(screen, (195, 144, 20), (0, 350, 600, 450))
     window(20, 20, 160, 220, day)
     window(280, 20, 200, 220, day)
-    cat(250, 360, 350, 200, day)
+    cat(250, 360, 350, 200, day, active_in_night=True)
     knit(260, 515, 10, screen)
-    cat(50, 550, 150, 105, day)
+    cat(150, 550, 200, 150, day, active_in_night=False)
 
-    knit(25, 25, 25, roll_knit)
-    roll_knit.set_colorkey((0, 0, 0))
-    screen.blit(pygame.transform.rotate(roll_knit, (-1)*counter_of_ticks * knit_speed), (counter_of_ticks * knit_speed, 600 - 50*sin(counter_of_ticks/3)))
+    rollknit(counter_of_ticks, knit_speed)
 
     if not day:
         screen.blit(night_light, (0, 0))
